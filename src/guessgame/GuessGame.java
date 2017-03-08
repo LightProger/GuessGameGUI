f9ee38edc0737063389aed7af1cb04cf5d289696
Alexandr <photoluxe@mail.ru>
2017-03-08 20:29:43 +0600

    Update

--- src/guessgame/GuessGame.java
+++ src/guessgame/GuessGame.java
@@ -2,23 +2,26 @@ package guessgame;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.text.MaskFormatter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.ParseException;
 
 public class GuessGame extends JFrame implements ActionListener {
 
 //        Переменные
     JPanel textdispPanel, textPanel, buttonPanel;
     JLabel numberLabel, textLabel;
-    JTextField textField;
-    JButton newGame;
+    JFormattedTextField textField;
+    JButton playGame;
     int number = 0;
     int displayValue = 0;
     String[] messsage = {"Поздравляем! Вы угадали!", "Может еще разок?"};
+    MaskFormatter formatter;
 
 //        Конструктор
-    public GuessGame() {
+    public GuessGame() throws ParseException {
         super("Угадай число");
 
 //        Cоздаем окно
@@ -38,16 +41,19 @@ public class GuessGame extends JFrame implements ActionListener {
         textdispPanel.add(numberLabel);
 
 //        Cоздаем поле ввода и добавляем его на панель textdispPanel
-        textField = new JTextField(1);
-        textField.setHorizontalAlignment(JTextField.RIGHT);
-        textField.setToolTipText("0123456789");
+        formatter = new MaskFormatter("*");
+        formatter.setValidCharacters("1234567890");
+        textField = new JFormattedTextField(formatter);
+        textField.setColumns(1);
+        textField.setHorizontalAlignment(JFormattedTextField.RIGHT);
+        textField.setToolTipText("1234567890");
         textdispPanel.add(textField);
 
 //        Создаем панель textPanel
         textPanel = new JPanel(new GridLayout(1, 1));
         textPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-//        Cоздаем тектовое поле, куда будут выводится сообщения и добавляем его на панель textPanel
+//        Cоздаем текстовое поле, куда будут выводится сообщения и добавляем его на панель textPanel
         textLabel = new JLabel();
         textLabel = new JLabel("", SwingConstants.CENTER);
         textLabel.setText("");
@@ -56,9 +62,9 @@ public class GuessGame extends JFrame implements ActionListener {
 //        Создаем панель для кнопки и добавляем на нее кнопку Играть
         buttonPanel = new JPanel(new GridLayout(1, 1));
         buttonPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-        newGame = new JButton("Играть");
-        newGame.addActionListener(this);
-        buttonPanel.add(newGame);
+        playGame = new JButton("Играть");
+        playGame.addActionListener(this);
+        buttonPanel.add(playGame);
 
 //        Добавляем все панели в окно
         this.add(textdispPanel, "North");
@@ -75,7 +81,7 @@ public class GuessGame extends JFrame implements ActionListener {
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         new GuessGame();
     }
 
@@ -84,41 +90,49 @@ public class GuessGame extends JFrame implements ActionListener {
 
 //        Создаем источник действия
         Object theButton = e.getSource();
+        try{
 
-//        Обрабатываем данные введеные в поле ввода
+//        Обрабатываем данные введенные в поле ввода
         String displayTextField = textField.getText();
 
         if (!"".equals(displayTextField)) {
-            try {
-                displayValue = Integer.parseInt(displayTextField);
-            } catch (Exception e1) {
-                JOptionPane.showMessageDialog(null, "Введите цифры!!!");
-                textField.setText(null);
+            displayValue = Integer.parseInt(displayTextField);
+
+            //        Действия при нажатии кнопки Играть
+            if (theButton == playGame){
+
+                while (true) {
+
+                    if (displayValue > number) {
+                        textLabel.setText("Ваше число больше загаданного. Попробуйте еще раз!");
+                    } else if (displayValue < number) {
+                        textLabel.setText("Ваше число меньше загаданного. Попробуйте еще раз!");
+                    } else if (displayValue == number) {
+
+                        // Окно сообщения
+                        int result = JOptionPane.showConfirmDialog(GuessGame.this,
+                                messsage, "Окно сообщения", JOptionPane.YES_NO_OPTION,
+                                JOptionPane.INFORMATION_MESSAGE);
+                        if (result == JOptionPane.YES_OPTION) {
+                            textField.setText(null);
+                            textLabel.setText(null);
+                            number = (int) (Math.random() * 10);
+                        } else if (result == JOptionPane.NO_OPTION) {
+                            System.exit(0);
+                        }
+                    }
+                    break;
+                }
+
             }
         }
+        }catch (Exception e1) {
+            JOptionPane.showMessageDialog(null, "Введите цифру!", "Ошибка",
+                    JOptionPane.ERROR_MESSAGE);
+            textField.setText(null);
+            textLabel.setText("Попробуйте еще раз!");
 
-//        Дейстия при нажатии кнопки Играть
-        if (theButton == newGame) {
-            while (true) {
-                if (displayValue > number) {
-                    textLabel.setText("Ваше число больше загаданого. Попробуйте еще раз!");
-                } else if (displayValue < number) {
-                    textLabel.setText("Ваше число меньше загаданого. Попробуйте еще раз!");
-                } else if (displayValue == number) {
-
-                    // Окно сообщения
-                    int result = JOptionPane.showConfirmDialog(GuessGame.this,
-                            messsage, "Окно сообщения", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE);
-                    if (result == JOptionPane.YES_OPTION) {
-                        textField.setText(null);
-                        textLabel.setText(null);
-                        number = (int) (Math.random() * 10);
-                    } else if (result == JOptionPane.NO_OPTION) {
-                        System.exit(0);
-                    }
-                }
-                break;
-            }
         }
     }
 }
+
